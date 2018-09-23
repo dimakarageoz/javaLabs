9c2c9b07049c1bf829c6b0c9ecc790c3281b7deb
dkarage <dmitriy.karageoz@qlearsite.com>
2018-09-23 22:36:34 +0300

    app: create base class

---
+++ README.md
@@ -0,0 +1,3 @@
+#### The current project contains labs for a Java course in university.
+FirstLab branch contains a folder with one ```FirstLab``` class.
+The program demonstrates functional of a basic system output tool
---
+++ src/FirstLab.java
@@ -0,0 +1,9 @@
+public class FirstLab {
+    public static void main(String[] args) {
+        FirstLab.authorInfo();
+    }
+
+    public static void authorInfo() {
+        System.out.println("Author: Dmitriy Karageoz \nGroup: AT-162 \nUniversity: ONPU \nGitHub: dimakarageoz");
+    }
+}
